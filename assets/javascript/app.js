54a8b718c78ac2bd6ff364689bb7c4b698356811
Ben Houston <BenjaminAHouston@gmail.com>
2018-09-21 21:06:24 -0400

    Created all of the elements that will be used for the game, and wrote a clearScreen function that removes all of the elements so the next page can be setup

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,76 +5,99 @@ $(document).ready(function() {
     //Add 1 correct answer string
     let questions = [
         {question: "What year was SpaceX founded?",
-         incorrect: ["1998","2007","2010"],
+         incorrect: ["1998", "2007", "2010"],
          correct: "2002"
         },
         {question: "How many engines are on a falcon heavy?",
-         incorrect: ["15","21","31"],
+         incorrect: ["15", "21", "31"],
          correct: "27"
         },
         {question: "Who is the richest man in the world?",
-         incorrect: ["Bill Gates","Elon Musk","Warren Buffet"],
+         incorrect: ["Bill Gates", "Elon Musk", "Warren Buffet"],
          correct: "Jeff Bezos"
         },
         {question: "How long was the Curiousity rover planned to last?",
-         incorrect: ["4 years","6 years","8 years"],
+         incorrect: ["4 years", "6 years", "8 years"],
          correct: "2 years"
         },
         {question: "How high is the ISS?",
-         incorrect: ["12 miles","87 miles","313 miles"],
+         incorrect: ["12 miles", "87 miles", "313 miles"],
          correct: "254 miles"
         },
         {question: "What was the diameter of Sputnik 1?",
-         incorrect: ["13 inches","33 inches","43 inches"],
+         incorrect: ["13 inches", "33 inches", "43 inches"],
          correct: "23 inches"
         },
         {question: "How much did Sputnik 1 weigh?",
-         incorrect: ["24 lbs","64 lbs","124 lbs"],
+         incorrect: ["24 lbs", "64 lbs", "124 lbs"],
          correct: "184 lbs"
         },
         {question: "Who was the first man in space?",
-         incorrect: ["Alan Shepard","Neil Armstrong","Laika Sirius"],
+         incorrect: ["Alan Shepard", "Neil Armstrong", "Laika Sirius"],
          correct: "Yuri Gagarin"
         },
         {question: "What was the name of the US program that put Alan Shepard into space?",
-         incorrect: ["Gemini","Apollo","Redstone"],
+         incorrect: ["Gemini", "Apollo", "Redstone"],
          correct: "Mercury"
         },
         {question: "What year did the first Space Shuttle launch?",
-         incorrect: ["1975","1986","1990"],
+         incorrect: ["1975", "1986", "1990"],
          correct: "1981"
         },
     ];
 
-    //------Create an interval variable
-
-    //------Create tracker variables
+    //------Create variables
+    //Create an interval variable
+    let interval;
     //Create a time variable
+    let time;
     //Create a # correct variable
+    let numCorrect;
     //Create a # incorrect variable
+    let numIncorrect;
     //Create a # unanswered variable
+    let numUnanswered;
 
     //------Create the elements for the starting page
     //Create the start button
+    let startButton = $("<h2>").addClass("answer").text("Start Game");
 
     //------Create the element for the timer display
+    let timerElem = $("<h2>Time Remaining: <span id='time'></span></h2>");
 
     //------Create the elements for question page
     //Create the header for the question
+    let questionElem = $("<h2>");
     //Create the headers for the answers
-    //Create event listener to the answers to answer the question
+    let answerElems = [
+        $("<h2>").addClass("answer"),
+        $("<h2>").addClass("answer"),
+        $("<h2>").addClass("answer"),
+        $("<h2>").addClass("answer")
+    ];
 
     //------Create the elements for the answered page
     //Create the header for whether the question was answered correctly or not,
     //  or time ran out
+    let answeredElem = $("<h2>");
     //Create the header for the correct answer
+    let correctAnswerElem = $("<h2>");
 
     //------Create the elements for the stats page
     //Create the header for affirming they are done
+    let affirmationElem = $("<h2>");
     //Create the headers for showing the number correct, incorrect,
     //  and unanswered.
+    let statsElems = [
+        $("<h2>"),
+        $("<h2>"),
+        $("<h2>")
+    ];
 
     //------Create a function that clears the elements from the screen
+    function clearScreen() {
+        $("#content").children().detach();
+    }
 
     //------Create a function that adds the elements for the start page
     //Add the start button
